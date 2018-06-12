550968582a4f7d21cf0030d176181552c9db01ba
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-12 19:36:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+unction theBeatlesPlay(musician, instruments) {
+  var array = []
+  for (let i = 0, l = musician.length; l>i; i++ ) {
+    array.push(`${musician[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var factsNew = []
+  let i = 0 
+  while (i < facts.length) {
+    factsNew.push(`${facts[i]}!!!`)
+    i++
+    
+  }
+  return factsNew
+}
+
+function iLoveTheBeatles(n) {
+  var array = []
+  
+  do { array.push("I Love the Beatles!")
+  i++  
+  } while (n < 15)
+}
