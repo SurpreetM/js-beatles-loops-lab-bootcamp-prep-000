539bb972404228e843a8089a52b87469a91f4722
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-12 19:42:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(n) {
   var array = []
-  if (n=<15) {
+  if (n < 15) {
   do { array.push("I Love the Beatles!")
   i++  
   } while (n < 15)
